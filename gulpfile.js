f8a28fb1f5a8d3078c6a20fdc34ed3145006b17e
Simon Legner <Simon.Legner@gmail.com>
2015-08-05 17:34:28 +0200

    Migrate to gulp build

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+/* jshint node: true, undef: true, strict: false */
+var fs = require('fs');
+var gulp = require('gulp');
+var karma = require('karma');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var header = require('gulp-header');
+var rename = require('gulp-rename');
+var del = require('del');
+var uglify = require('gulp-uglify');
+
+var config = {
+  pkg: JSON.parse(fs.readFileSync('./package.json')),
+  banner:
+      '/*!\n' +
+      ' * <%= pkg.name %>\n' +
+      ' * <%= pkg.homepage %>\n' +
+      ' * Version: <%= pkg.version %> - <%= timestamp %>\n' +
+      ' * License: <%= pkg.license %>\n' +
+      ' */\n'
+};
+
+gulp.task('default', ['build','test']);
+gulp.task('build', ['scripts']);
+gulp.task('test', ['build', 'karma']);
+
+gulp.task('watch', ['build','karma-watch'], function() {
+  gulp.watch(['src/**/*.{js,html}'], ['build']);
+});
+
+gulp.task('clean', function(done) {
+  del(['dist'], done);
+});
+
+gulp.task('scripts', ['clean'], function() {
+  return gulp.src(['src/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'))
+    .pipe(concat('ngMomentInput.js'))
+    .pipe(header(config.banner, {
+      timestamp: (new Date()).toISOString(),
+      pkg: config.pkg
+    }))
+    .pipe(gulp.dest('dist'))
+    .pipe(uglify({preserveComments: 'some'}))
+    .pipe(rename({extname:'.min.js'}))
+    .pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('karma', ['build'], function(done) {
+  runTests(true, done);
+});
+
+gulp.task('karma-watch', ['build'], function(done) {
+  runTests(false, done);
+});
+
+function runTests(singleRun, done) {
+  new karma.Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: singleRun,
+    autoWatch: !singleRun
+  }, function() {
+    done();
+  }).start();
+}
--- karma.conf.js
+++ karma.conf.js
@@ -3,26 +3,16 @@ module.exports = function(config) {
 
   config.set({
 
-    basePath: '../',
     files: [
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/moment/moment.js',
-      'src/*.js',
-      'test/unit/**/*.js'
+      'dist/*.min.js',
+      'test/*.js'
     ],
 
-    logLevel: config.LOG_INFO,
-    colors: true,
-
-    autoWatch: true,
     frameworks: ['jasmine'],
-    browsers: ['Chrome'],
-
-    junitReporter: {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+    browsers: ['Firefox']
 
   });
 };
